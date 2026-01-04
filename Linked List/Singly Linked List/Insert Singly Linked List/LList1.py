3fa2f72101ea4675e80b1d38bd4578801a14540c
MOHAMMED RAFIQUIE AHMED <158809811+RafiquieAhmed@users.noreply.github.com>
2026-01-05 00:14:45 +0530

    Add files via upload

---
+++ Linked List/Singly Linked List/Insert Singly Linked List/LList1.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self,info,next=None):
+        self.data=info
+        self.next=next
+        
+class SinglyLinkedList:
+    def __init__(self,head=None): #pointer assine
+        self.head=head
+        
+    def insertAtEnd(self,value):
+        temp=Node(value)
+        # if already create 
+        if(self.head != None):
+            new_Node=self.head
+            while(new_Node.next !=None):
+                new_Node=new_Node.next
+            new_Node.next =temp
+        
+        # if not  created
+        else:
+            self.head=temp
+            
+            #print data
+      
+    def inserAtBegining(self,value):
+        temp =Node(value)
+        temp.next=self.head
+        self.head=temp
+        
+    #insert at middle
+    def insertInMid(self,value,x): 
+        #x is searching element
+        temp=Node(value)
+        t1=self.head
+        
+        while(t1.next !=None):
+            if(t1.data ==x):
+                temp.next=t1.next
+                t1.next =temp
+            t1 =t1.next
+            
+            
+        
+    
+          
+    def printLL(self):
+        new_Node=self.head
+        while(new_Node.next !=None):
+            print(new_Node.data)
+            new_Node=new_Node.next
+        print(new_Node.data)
+        
+obj =SinglyLinkedList()
+obj.insertAtEnd(10)
+obj.insertAtEnd(20)
+obj.insertAtEnd(30)
+obj.inserAtBegining(5)
+obj.insertInMid(40,20)
+obj.printLL()
+
