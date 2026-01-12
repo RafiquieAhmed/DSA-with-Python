a18744de62e332a1a811a38a440c4247c33546ef
MOHAMMED RAFIQUIE AHMED <158809811+RafiquieAhmed@users.noreply.github.com>
2026-01-12 22:38:16 +0530

    Add files via upload

---
+++ Trees/Tree Traversal/inorder.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,value):
+        self.left =None
+        self.right=None
+        self.data=value
+    
+def InOrder(root):
+    if( root != None):
+        InOrder(root.left)
+        print(root.data,end=" ")
+        InOrder(root.right)
+    
+
+root=Node(1)
+root.left =Node(3)
+root.right =Node(5)
+root.left.left =Node(2)
+root.left.right =Node(4)
+root.right.right=Node(8)
+InOrder(root)
